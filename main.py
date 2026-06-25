71a8772e1e8b66bb534a96cc17901d67e27d5a7a
menard <menardddddd@gmail.com>
2026-06-25 11:49:24 +0800

    implemented lifespan for creating tables

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import model
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from database import Base, engine
+from api import router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    await engine.dispose()
+
+
+app = FastAPI(title="Apex Hunter", lifespan=lifespan)
+
+app.include_router(router)
